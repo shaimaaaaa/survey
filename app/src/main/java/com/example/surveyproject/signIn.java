5c0ae89a52d6b7bf45ad633abcb766b53daba3e4
shimaa <shaimafif76@gmail.com>
2023-06-20 15:37:10 +0300

    start

---
+++ app/src/main/java/com/example/surveyproject/signIn.java
@@ -0,0 +1,13 @@
+package com.example.surveyproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
